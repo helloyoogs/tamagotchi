032fe15d1d0f25af0a67bb566065ea77b8c3a7b3
육희영 <helloyoogs@naver.com>
2025-06-05 14:26:29 +0900

    [FE] 공통 인풋 구현

---
+++ frontend/src/app/component/_atoms/cInput.tsx
@@ -0,0 +1,68 @@
+import {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  InputHTMLAttributes,
+} from "react";
+
+interface CInputContextProps {
+  error?: boolean;
+}
+const CInputContext = createContext<CInputContextProps | undefined>(undefined);
+
+interface FieldProps {
+  children: ReactNode;
+  error?: boolean;
+  className?: string;
+}
+
+const Field: FC<FieldProps> = ({ children, error, className }) => {
+  return (
+    <CInputContext.Provider value={{ error }}>
+      <div className={`flex flex-col gap-[4px] flex-1 ${className ?? ""}`}>
+        {children}
+      </div>
+    </CInputContext.Provider>
+  );
+};
+
+interface LabelProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Label: FC<LabelProps> = ({ children, className }) => (
+  <label className={className}>{children}</label>
+);
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+const Input: FC<InputProps> = ({ className = "", ...props }) => {
+  const context = useContext(CInputContext);
+  const error = context?.error ?? false;
+
+  return (
+    <input
+      {...props}
+      className={`cInput ${error ? "border-[#d32f2f]" : ""} ${className}`}
+    />
+  );
+};
+
+interface ValidMessageProps {
+  children: ReactNode;
+}
+
+const ValidMessage: FC<ValidMessageProps> = ({ children }) => {
+  const context = useContext(CInputContext);
+  const error = context?.error ?? false;
+
+  return error ? <div className="cInputValidMessage">{children}</div> : null;
+};
+
+export const CInput = Object.assign(Input, {
+  Field,
+  Label,
+  ValidMessage,
+});
--- frontend/src/app/containers/signup/page.tsx
+++ frontend/src/app/containers/signup/page.tsx
@@ -1,23 +1,67 @@
+"use client";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { CInput } from "@/app/component/_atoms/cInput";
+import { UserApi } from "@/app/@types/api";
+
 export default function SignUp() {
+  const [form, setForm] = useState<UserApi>({
+    email: "",
+    password: "",
+    nickname: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await fetch("/api/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    });
+
+    alert("회원가입 요청이 전송되었습니다!");
+  };
+
+  const fields: {
+    name: keyof UserApi;
+    label: string;
+    type?: string;
+    placeholder: string;
+  }[] = [
+    { name: "email", label: "아이디(이메일)", placeholder: "이메일" },
+    {
+      name: "password",
+      label: "비밀번호",
+      type: "password",
+      placeholder: "비밀번호",
+    },
+    { name: "nickname", label: "닉네임", placeholder: "닉네임" },
+  ];
+
   return (
-    <form aria-label="회원가입 폼">
-      <div>
-        <label htmlFor="id">ID</label>
-        <input type="text" id="id" name="id" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" required />
-      </div>
-      <div>
-        <label htmlFor="nickname">Nickname</label>
-        <input type="text" id="nickname" name="nickname" required />
-      </div>
-      <button type="submit">Sign Up</button>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-4 w-[600px] m-auto border border-[#DBDBDB] p-[30px] mt-[10%]"
+    >
+      <p className={"mb-[50px] text-center text-[20px]"}>TAMAGOTCHI</p>
+      {fields.map(({ name, label, type = "text", placeholder }) => (
+        <CInput.Field key={name} error={true}>
+          <CInput.Label>{label}</CInput.Label>
+          <CInput
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            value={form[name]}
+            onChange={handleChange}
+          />
+          <CInput.ValidMessage>asdasdsadasdsa</CInput.ValidMessage>
+        </CInput.Field>
+      ))}
+      <button type="submit">회원가입</button>
     </form>
   );
 }
