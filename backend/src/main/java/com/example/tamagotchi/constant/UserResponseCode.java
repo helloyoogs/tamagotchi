40272e1da4c13c481117026359cf775d88d74176
육희영 <helloyoogs@naver.com>
2025-06-11 16:22:58 +0900

    [BE] user 메시지 캡슐화

---
+++ backend/src/main/java/com/example/tamagotchi/constant/UserResponseCode.java
@@ -0,0 +1,10 @@
+package com.example.tamagotchi.constant;
+
+public class UserResponseCode {
+    public static final String EMAIL_ALREADY_EXISTS = "EMAIL_ALREADY_EXISTS";
+    public static final String NICKNAME_ALREADY_EXISTS = "NICKNAME_ALREADY_EXISTS";
+    public static final String SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+    public static final String LOGIN_SUCCESS = "LOGIN_SUCCESS";
+    public static final String INVALID_CREDENTIALS = "INVALID_CREDENTIALS";
+    public static final String USER_NOT_FOUND = "USER_NOT_FOUND";
+}
---
+++ backend/src/main/java/com/example/tamagotchi/constant/UserValidationCode.java
@@ -0,0 +1,12 @@
+package com.example.tamagotchi.constant;
+
+public class UserValidationCode {
+    public static final String EMAIL_REQUIRED = "EMAIL_REQUIRED";
+    public static final String EMAIL_INVALID = "EMAIL_INVALID";
+    public static final String PASSWORD_REQUIRED = "PASSWORD_REQUIRED";
+    public static final String PASSWORD_LENGTH_INVALID = "PASSWORD_LENGTH_INVALID";
+    public static final String PASSWORD_WEAK = "PASSWORD_WEAK";
+    public static final String NICKNAME_REQUIRED = "NICKNAME_REQUIRED";
+    public static final String NICKNAME_LENGTH_INVALID = "NICKNAME_LENGTH_INVALID";
+    public static final String NICKNAME_INVALID = "NICKNAME_INVALID";
+}
